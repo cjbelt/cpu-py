f99b2018d6cd3b23e884c63bd95d43f76d3485d0
cjbelt <123838557+cjbelt@users.noreply.github.com>
2026-05-28 23:50:30 -0300

    caminho para diretorio info adicionado

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import os
 import sys
 
 diretorio_raiz = os.path.abspath(os.path.dirname(__file__))
+diretorio_info = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "info"))
 
 if diretorio_raiz not in sys.path:
     sys.path.insert(0, diretorio_raiz)
+
+if diretorio_info not in sys.path:
+    sys.path.insert(0, diretorio_info)
 
 from src.ui.interface import MonitorApp
 
